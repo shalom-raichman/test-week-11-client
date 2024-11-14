21347f5dba57f2cf250426eb6fe9ca6950fc7c1a
Shalom <shlomo000@gmail.com>
2024-11-14 14:11:33 +0200

    change to redux from user context

--- src/App.tsx
+++ src/App.tsx
@@ -3,41 +3,52 @@ import Login from './components/auth/Login'
 import Register from './components/auth/Register'
 import { IUser } from './types/models/user.model'
 import Nav from './components/Nav'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import Terorists from './components/pages/Terorists'
 import Inteseptors from './components/pages/Inteseptors'
 import { OrgnizationsEnum } from './types/enums/orgnizationEnum'
-import { IResource } from './types/models/resource.model'
+import { io } from 'socket.io-client'
+import { Provider } from 'react-redux'
 
-export const UserContext = createContext<any>(null)
+
+export const UserContext = createContext<
+  { user: IUser; setUser: (x: IUser) => void } | undefined
+>(undefined)
 
 const App = () => {
-  // { user: IUser | null, setUser: (x: IUser) => void } | null
   // const initialUser: IUser = {
-  //   name: '',
-  //   password: '',
+  //   name: 'shalom2',
+  //   password: '$2b$10$Tfj3TzmxEtkxrB6mu8v8c.nhpogBPUKFhVwdl/LHhzdgeVUUIsYMS',
   //   orgnization: {
-  //     name: '' as OrgnizationsEnum,
-  //     resources: [] as IResource[],
-  //     budget: 0
-  //   }
+  //     name: OrgnizationsEnum.Hezbollah,
+  //     resources: [
+  //       {
+  //         name: 'Iron Dome',
+  //         amount: 25,
+  //       },
+  //       {
+  //         name: "David's Sling",
+  //         amount: 15,
+  //       },
+  //     ],
+  //     budget: 8000000,
+  //   },
   // }
-
-  const [user, setUser] = useState<IUser | null>()
+  // const [user, setUser] = useState<IUser>(initialUser)
 
   return (
-    <UserContext.Provider value={{ user, setUser }}>
+    // <UserContext.Provider value={{ user, setUser }}>
       <div className='app'>
         <Nav />
         <Routes>
-          <Route path='/' element={<Login />} />
+          <Route path='/' element={<Navigate to='/login' />} />
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
           <Route path='/terorists' element={<Terorists />} />
           <Route path='/interseptors' element={<Inteseptors />} />
         </Routes>
       </div>
-    </UserContext.Provider>
+    // </UserContext.Provider>
   )
 }
 
