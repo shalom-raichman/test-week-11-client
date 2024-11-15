5dec6403403ccbf4294db7df476d62ea7c26979c
Shalom <shlomo000@gmail.com>
2024-11-15 13:12:37 +0200

    add intrception logic

--- src/components/pages/Intrcptors/InterceptionBtn.tsx
+++ src/components/pages/Intrcptors/InterceptionBtn.tsx
@@ -1,7 +1,28 @@
+import { socket } from '../../../main'
+import { fetchMissileLaunchStatus } from '../../../redux/missileSlice'
+import { useAppDispatch } from '../../../redux/store'
+import { MissileLaunchStatusEnum } from '../../../types/enums/MissileLaunchStatusEnum'
 
-const InterceptionBtn = () => {
+interface Props {
+  _id: string
+}
+
+const InterceptionBtn = ({ _id }: Props) => {
+  const dispatch = useAppDispatch()
+
+  const handelIntercetion = () => {
+    dispatch(
+      fetchMissileLaunchStatus({
+        _id,
+        status: MissileLaunchStatusEnum.Intercepted,
+      })
+    )
+    socket.emit('interception_launch')
+  }
   return (
-    <div>InterceptionBtn</div>
+    <button className='btn btn-danger' onClick={handelIntercetion}>
+      Intercept
+    </button>
   )
 }
 
