cc7632f0c1e3ebb3d97b2bbfd993aec8f5ce8bb3
Shalom <shlomo000@gmail.com>
2024-11-17 17:39:58 +0200

    add rows to the tabls

--- src/components/pages/TableRow.tsx
+++ src/components/pages/TableRow.tsx
@@ -1,4 +1,5 @@
 import { useAppSelector } from '../../redux/store'
+import { MissileLaunchStatusEnum } from '../../types/enums/MissileLaunchStatusEnum'
 import { MissileLaunch } from '../../types/models/missileLaunch.model'
 import InterceptionBtn from './Intrcptors/InterceptionBtn'
 
@@ -7,15 +8,20 @@ interface Props {
 }
 
 const TableRow = ({ launch }: Props) => {
-  const user = useAppSelector(s=>s.user.user)
+  const user = useAppSelector((s) => s.user.user)
   return (
     <tbody>
       <tr>
         <td>{launch.rocketType}</td>
         <td>{launch.timeToHit}</td>
+        <td>{launch.launchFrom}</td>
+        <td>{launch.launchTo}</td>
         <td>
           {launch.status}
-          {user.orgnization.name.split(' ')[0] === 'IDF' && <InterceptionBtn _id={launch._id as string}/>}
+          {user.orgnization.name.split(' ')[0] === 'IDF' &&
+            launch.status == MissileLaunchStatusEnum.Launched && (
+              <InterceptionBtn _id={launch._id as string} />
+            )}
         </td>
       </tr>
     </tbody>
